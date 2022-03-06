c75ff2f84abda5039aaab028efa81c2afcff362a
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-03-06 11:35:05 -0500

    adaptador simple

---
+++ app/src/main/java/com/example/ejemplolistview/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ejemplolistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    var frutas:ArrayList<String> = ArrayList()
+
+        frutas.add("Manzana")
+        frutas.add("Durazno")
+        frutas.add("Platano")
+        frutas.add("Sandía")
+
+        val lista= findViewById<ListView>(R.id.lista)
+        val adaptador =ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,frutas)
+        lista.adapter=adaptador
+        lista.onItemClickListener=AdapterView.OnItemClickListener{parent, view,position,id ->
+            Toast.makeText(this,frutas.get(position),Toast.LENGTH_LONG).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ejemplolistview/fruta.kt
@@ -0,0 +1,13 @@
+package com.example.ejemplolistview
+
+import android.media.Image
+class fruta (nombre:String, imagen:Image){
+    var nombre:String=""
+    var imagen:Image?=null
+
+    init{
+        this.nombre=nombre
+        this.imagen=imagen
+    }
+
+}
